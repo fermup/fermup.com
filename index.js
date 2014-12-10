c63f9b4884087b5c623f6c4d3ea7cf1c8fb1b7f9
Branden Byers <branden@Air.local>
2014-12-09 22:38:16 -0600

    Add plugins

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
-var Metalsmith  = require('metalsmith'),
-    markdown    = require('metalsmith-markdown'),
-    templates   = require('metalsmith-templates'),
-    collections = require('metalsmith-collections'),
-    permalinks  = require('metalsmith-permalinks'),
-    Handlebars  = require('handlebars'),
-    fs          = require('fs'),
-    watch       = require('metalsmith-watch'),
-    serve       = require('metalsmith-serve');
+var Metalsmith    = require('metalsmith'),
+    markdown      = require('metalsmith-markdown'),
+    templates     = require('metalsmith-templates'),
+    collections   = require('metalsmith-collections'),
+    permalinks    = require('metalsmith-permalinks'),
+    drafts        = require('metalsmith-drafts'),
+    Handlebars    = require('handlebars'),
+    fs            = require('fs'),
+    autoprefixer  = require('metalsmith-autoprefixer'),
+    cleanCSS      = require('metalsmith-clean-css'),
+    uglify        = require('metalsmith-uglify'),
+    htmlescape    = require('metalsmith-htmlescape'),
+    watch         = require('metalsmith-watch'),
+    serve         = require('metalsmith-serve');
 
 
 Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
 Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
 
 Metalsmith(__dirname)
+    .use(autoprefixer())
+    .use(drafts())
     .use(collections({
         pages: {
-            pattern: 'content/pages/*.md'
+            pattern: '/content/pages/*.md'
         },
         podcast: {
             pattern: 'content/podcast/*.md',
             sortBy: 'date',
             reverse: true
-        }
+        },
+        blog: {
+            pattern: 'content/blog/*.md',
+            sortBy: 'date',
+            reverse: true
+        },
+        tutorials: {
+            pattern: 'content/tutorials/*.md',
+            sortBy: 'date',
+            reverse: true
+        },
+        recipes: {
+            pattern: 'content/recipes/*.md',
+            sortBy: 'date',
+            reverse: true
+     }
     }))
     .use(markdown())
     .use(permalinks({
-        pattern: ':collection/:title'
+        pattern: ':collection/:title',
+        relative: false
     }))
     .use(templates('handlebars'))
+    .use(cleanCSS())
+    .use(uglify())
+    .use(htmlescape())
     .use(watch())
     .use(serve())
     .destination('./build')
---
+++ src/content/pages/contact.md
@@ -0,0 +1,34 @@
+---
+title: About
+template: page.hbt
+---
+# This is me.
+
+Ipsum quam fugit expedita nesciunt eos maiores nulla quasi porro ratione recusandae ullam animi quae totam dolore. Corporis voluptate ab dolores iure amet nostrum. Adipisci libero fuga et voluptatibus quos?
+
+## This & is a header
+
+Ipsum tenetur earum labore architecto unde libero. Fugiat nisi assumenda magnam ut commodi animi deleniti vitae quasi eius quis ipsum. Dolor eius aspernatur iusto itaque sunt nesciunt inventore. Maxime doloribus quasi quasi dolor iste! Eius eveniet harum quos cumque eaque necessitatibus hic nam nisi animi, non, voluptate nostrum consequuntur. Sit repudiandae dolores quas incidunt vero a et. Nesciunt ipsum eligendi officiis placeat quos amet delectus voluptatem quisquam pariatur magnam fuga unde beatae vel. Esse quibusdam pariatur consequuntur pariatur est.
+
+## This is another header
+
+Elit non hic sit incidunt sit debitis ratione consequatur dolorem esse adipisci?
+Tempora minus ipsum officiis harum doloribus aliquid. Quas deserunt laborum in
+dignissimos hic aliquid. Tenetur et dignissim. Sit enim beatae itaque accusamus qui placeat. Eligendi debitis ea ipsa asperiores temporibus numquam? Distinctio laboriosam ducimus doloribus aut quos nemo. Hic hic quod nobis harum incidunt repellat iusto! Nobis. Sit illum delectus explicabo minima nam. Quo doloribus aperiam nam ipsa accusamus. Incidunt laborum quos nobis eius unde. Est libero veritatis delectus ratione quam, corrupti! Vitae molestias consectetur explicabo laboriosam.
+
+<div class="image-full-width"><img src="http://lorempixel.com/1200/400/food"></div>
+
+###This is a smaller header
+
+Dolor doloremque provident ducimus optio sit distinctio consequuntur delectus! Eum facilis a quo quasi dicta nihil libero facilis tempore nihil asperiores id dicta debitis aperiam unde? Fugiat culpa quibusdam nostrum.
+
+* These are some bullets
+* yo
+* Yes, that is what I said.
+
+<div class="image-full-width"><img src="/img/social-instagram.svg" width="80px"></div>
+
+Thank you for reading this and I hope you keeping reading in the future. Thank
+you very much.
+
+---
